8dfd297489087d116ad3a9ada52554bee0d6ac22
sofia <17679131335@163.com>
2021-06-11 23:00:25 +0800

    提交到本地

---
+++ nacos-restful-consumer/src/main/java/com/itheima/nacos/consumer/RestConsumerVBootstrap.java
@@ -0,0 +1,12 @@
+package com.itheima.nacos.consumer;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestConsumerVBootstrap {
+    public static void main(String[] args) {
+        SpringApplication.run(RestConsumerVBootstrap.class,args);
+    }
+}
---
+++ nacos-restful-provider/src/main/java/com/itheima/nacos/provider/RestProviderVBootstrap.java
@@ -0,0 +1,12 @@
+package com.itheima.nacos.provider;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestProviderVBootstrap {
+    public static void main(String[] args) {
+        SpringApplication.run(RestProviderVBootstrap.class,args);
+    }
+}
